acfcaf601d251906e3560b83379ce920150f3024
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-10-23 20:25:06 +0000

    Server config

--- r-code-for-bp-alert.R
+++ r-code-for-bp-alert.R
@@ -4,13 +4,12 @@
 
 #THis code assumes that all patient recent BP data is available and ordered into the file
 
-setwd("/Users/isabelsassoon/Documents/CONSULT/case study june 18")
+#setwd("/Users/isabelsassoon/Documents/CONSULT/case study june 18")
 
 library(ggplot2)
 
-
 bp.check<-function(nn){
-  bp<-read.csv("bp-cs-p123.csv")
+  bp<-read.csv("data/bp-cs-p123.csv")
   past<-head(bp, n=nn)
   p1<-mean(past$sys)
   recent<-tail(bp, n=nn)
@@ -25,7 +24,7 @@ bp.check<-function(nn){
 
 bp.trend<-bp.check(7)
 
-bp<-read.csv("bp-cs-p123.csv")
+bp<-read.csv("data/bp-cs-p123.csv")
 #creating the additional information from the patient stats
 patient.id<-toString(bp$pid[[1]])
 
@@ -37,10 +36,10 @@ last.dia<-recent$dia
 patient.facts<-data.frame(patient.id,bp.trend, last.sys, last.dia)
 
 #read in the ehr data (this is a dummy file for now)
-ehr<-read.csv("ehr-cs-p123.csv")
+ehr<-read.csv("data/ehr-cs-p123.csv")
 patient.ehr.facts<-data.frame(patient.facts,ehr)
 
-write.csv(patient.ehr.facts, file="patient-facts.csv", row.names = FALSE)
+write.csv(patient.ehr.facts, file="output/patient-facts.csv", row.names = FALSE)
 
 
 #convert to json
@@ -49,7 +48,7 @@ library(jsonlite)
 x<-toJSON(patient.ehr.facts)
 cat(x)
 
-write_json(x,path = "patient-facts.json")
+write_json(x,path = "output/patient-facts.json")
 
 
 #future improvements:
@@ -58,7 +57,7 @@ write_json(x,path = "patient-facts.json")
 
 
 bp.plot<-function(){
-  bp<-read.csv("bp-cs-p123.csv")
+  bp<-read.csv("data/bp-cs-p123.csv")
   ggplot(bp, aes(seq, sys)) + geom_line() +  xlab("Days") + ylab("BP Systolic")+stat_smooth(method = "loess")
   
   p = ggplot() + 
