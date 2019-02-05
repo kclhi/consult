85d6faf90e1c829db5446baf0b79875c4778499f
Martin Chapman <contact@martinchapman.co.uk>
2019-02-05 18:26:52 +0000

    FHIR put endpoint receiver from HAPI FHIR server. Parse FHIR+JSON body encoding.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.put('/:type/:id', function(req, res, next) {
+
+  console.log(req.body);
+  res.sendStatus(200);
+
 });
 
 module.exports = router;
