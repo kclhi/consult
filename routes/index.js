592a2d625861cef7a74ee9ecc6b2d829d2c07d66
Martin Chapman <contact@martinchapman.co.uk>
2019-02-06 00:58:19 +0000

    Inform data miner of new (BP) readings.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../lib/config');
+var request = require('request');
 
-router.put('/:type/:id', function(req, res, next) {
+router.put('/Observation/:id', function(req, res, next) {
 
-  console.log(req.body);
-  res.sendStatus(200);
+  headers = [];
+  row = [];
+
+  try {
+
+    req.body.component.forEach(function(measure) {
+
+      // 'c' added (code) as in R colum name references cannot be numerical.
+      headers.push("c" + measure["code"].coding[0].code);
+      row.push(measure["valueQuantity"].value);
+
+    });
+
+  } catch(error) {
+
+    console.log(error);
+
+  }
+
+  // TODO: Determine miner endpoint based on content of FHIR resource.
+  request.post(config.DATA_MINER_URL + "/check/bp", {
+      json: {
+        "nn": "7",
+        "csv": headers.toString() + "\n" + row.toString(),
+      },
+  },
+  function (error, response, body) {
+
+      if (!error && response.statusCode == 200) {
+
+           console.log(response.body)
+
+      } else {
+
+           console.log(error)
+
+      }
+
+      res.sendStatus(200);
+
+   });
+
 
 });
 
