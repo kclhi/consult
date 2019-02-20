e06852e3b31d00a4b5929bca26e7485af5d1f4a6
Martin Chapman <contact@martinchapman.co.uk>
2019-02-20 22:56:35 +0000

    Provenance library + test.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
+var provenanceRouter = require('./routes/provenance');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/provenance', provenanceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
