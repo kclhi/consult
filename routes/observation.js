006261304c070c2c31498c76649ec4bcda2affc8
Martin Chapman <contact@martinchapman.co.uk>
2019-02-28 18:11:06 +0000

    Send HR data to the miner.

--- routes/observation.js
+++ routes/observation.js
@@ -23,174 +23,219 @@ function populateProvenanceTemplateBP(pid, code, value, callback) {
 
 }
 
-router.put('/:id', function(req, res, next) {
+function getPatientStats(patientID, callback) {
 
-  // Use code to determine type of observation.
-  if ( req.body.code.coding[0].code == "8867-4" ) {
+  patientHeaders = [];
+  patientRow = [];
 
-    res.sendStatus(200);
-    return;
+  utils.callFHIRServer("Patient/" + patientID, "", function(patientData) {
+
+    patientHeaders.push("birthDate");
+    patientRow.push(JSON.parse(patientData).birthDate);
+
+    patientHeaders.push("ethnicity");
+    patientRow.push(JSON.parse(patientData).extension[0].extension[0].valueCoding.display);
+
+    utils.callFHIRServer("MedicationDispense", "subject=" + patientID, function(medicationDispenseData) {
+
+      // TODO: Remove async.
+      async.each(JSON.parse(medicationDispenseData).entry, function(medicationDispense, callback) {
+
+        var medication = 1;
+
+        utils.callFHIRServer(medicationDispense.resource.medicationReference.reference, "", function(medicationData) {
+
+          patientHeaders.push("medication" + medication)
+          patientRow.push(JSON.parse(medicationData)['code']['coding'][0].display);
+          medication += 1;
+          callback();
+
+        });
+
+      }, function(medicationDispenseDataError) {
+
+        utils.callFHIRServer("Condition", "subject=" + patientID, function(conditionData) {
+
+          var problem = 1;
+
+          JSON.parse(conditionData).entry.forEach(function(condition) {
+
+            patientHeaders.push("problem" + problem)
+            problem += 1;
+            patientRow.push(condition.resource.code.coding[0].display);
+
+          });
+
+          callback(patientHeaders, patientRow);
+
+        });
+
+      });
+
+    });
+
+  });
+
+}
+
+function sendAlert(response, alertField, alertValue, callback) {
+
+  var minutesSinceLastAlert = Number.MAX_SAFE_INTEGER;
+
+  if ( lastAlert ) {
+
+    minutesSinceLastAlert = Math.floor(((new Date().getTime() - lastAlert.getTime()) / 1000) / 60);
 
   }
 
-  bpHeaders = [];
-  patientHeaders = [];
-  bpRow = [];
-  patientRow = [];
+  // TODO: Miner response is oddly nested.
+  minerResponse = JSON.parse(response.body[0]);
+
+  // TODO: Generic term that indicates the issue, and potentially indicates which dialogue to start.
+  if ( response.body && minerResponse[0][alertField].indexOf(alertValue) > -1 && minutesSinceLastAlert > config.MAX_ALERT_PERIOD) {
+
+    request({
+      method: "POST",
+      url: config.DIALOGUE_MANAGER_URL + "/dialogue/initiate",
+      headers: {
+       "Authorization": "Basic " + new Buffer(config.USERNAME + ":" + config.PASSWORD).toString("base64")
+      },
+      json: {
+       // TODO: Something from the data miner response that indicates which dialogue to initiate.
+       "dialogueID": "2",
+       // TODO: Assume username on chat is the same as Patient ID in FHIR or query a service storing a mapping between the two.
+       "username": "user",
+      }
+     },
+     function (error, response, body) {
+
+       console.log("Dialogue manager: " + response.statusCode);
+
+       if (!error && response.statusCode == 200) {
+
+          console.log(response.statusCode);
+          lastAlert = new Date();
+          callback(200);
+
+       } else {
+
+          console.log(error);
+          callback(400);
+
+       }
+
+     });
+
+   } else {
+
+     callback(200);
+
+   }
+
+}
+
+function processObservation(req, res, callback) {
+
+  observationHeaders = [];
+  observationRow = [];
 
   const patientID = req.body.subject.reference.replace("Patient/", "");
 
   // Get patient stats
-  bpHeaders.push("pid");
-  bpRow.push(patientID);
+  observationHeaders.push("pid");
+  observationRow.push(patientID);
 
-  utils.callFHIRServer("Patient/" + patientID, "", function(patientData) {
+  getPatientStats(patientID, function(patientHeaders, patientRow) {
 
-      patientHeaders.push("birthDate");
-      patientRow.push(JSON.parse(patientData).birthDate);
+    // Get observation stats
+    req.body.component.forEach(function(measure) {
 
-      patientHeaders.push("ethnicity");
-      patientRow.push(JSON.parse(patientData).extension[0].extension[0].valueCoding.display);
+      // 'c' added (code) as in R colum name references cannot be numerical (bad practice too).
+      const code = "c" + measure["code"].coding[0].code;
+      const value = measure["valueQuantity"].value;
+      observationHeaders.push(code);
+      observationRow.push(value);
+      populateProvenanceTemplateBP(patientID, code, value, function(response) {});
 
-      utils.callFHIRServer("MedicationDispense", "subject=" + patientID, function(medicationDispenseData) {
+    });
 
-          // TODO: Remove async.
-          async.each(JSON.parse(medicationDispenseData).entry, function(medicationDispense, callback) {
+    callback(observationHeaders, observationRow, patientHeaders, patientRow);
 
-              var medication = 1;
+  });
 
-              utils.callFHIRServer(medicationDispense.resource.medicationReference.reference, "", function(medicationData) {
+}
 
-                  patientHeaders.push("medication" + medication)
-                  patientRow.push(JSON.parse(medicationData)['code']['coding'][0].display);
-                  medication += 1;
-                  callback();
+router.put('/:id', function(req, res, next) {
 
-              });
+  // Use code to determine type of observation.
+  if ( req.body.code.coding[0].code == config.HR_CODE ) {
 
-          }, function(medicationDispenseDataError) {
+    processObservation(req, res, function(observationHeaders, observationRow, patientHeaders, patientRow) {
 
-              utils.callFHIRServer("Condition", "subject=" + patientID, function(conditionData, callback) {
+      request.post(config.DATA_MINER_URL + "/check/hr", {
+        json: {
+          "hr": observationHeaders.toString() + "\n" + observationRow.toString(),
+          "nn": "7",
+          "ehr": patientHeaders.toString() + "\n" + patientRow.toString()
+        },
+      },
+      function (error, response, body) {
 
-                  var problem = 1;
+        console.log("Data miner: " + response.statusCode);
 
-                  JSON.parse(conditionData).entry.forEach(function(condition) {
+        if (!error && response.statusCode == 200) {
 
-                      patientHeaders.push("problem" + problem)
-                      problem += 1;
-                      patientRow.push(condition.resource.code.coding[0].display);
+          console.log(response);
+          res.sendStatus(200);
 
-                  });
+        } else {
 
-                  // Get observation stats
-                  req.body.component.forEach(function(measure) {
+          console.log(error);
+          res.sendStatus(400);
 
-                      // 'c' added (code) as in R colum name references cannot be numerical.
-                      const code = "c" + measure["code"].coding[0].code;
-                      const value = measure["valueQuantity"].value;
-                      bpHeaders.push(code);
-                      bpRow.push(value);
-                      populateProvenanceTemplateBP(patientID, code, value, function(response) {});
-
-                  });
-
-                  // TODO: Determine miner endpoint based on content of FHIR resource.
-                  request.post(config.DATA_MINER_URL + "/check/bp", {
-                      json: {
-                        "bp": bpHeaders.toString() + "\n" + bpRow.toString(),
-                        "nn": "7",
-                        "ehr": patientHeaders.toString() + "\n" + patientRow.toString()
-                      },
-                  },
-                  function (error, response, body) {
-
-                      console.log("Data miner: " + response.statusCode);
-
-                      if (!error && response.statusCode == 200) {
-
-                          var minutesSinceLastAlert = Number.MAX_SAFE_INTEGER;
-
-                          if ( lastAlert ) {
-
-                              minutesSinceLastAlert = Math.floor(((new Date().getTime() - lastAlert.getTime()) / 1000) / 60);
-
-                          }
-
-                          // TODO: Miner response is oddly nested.
-                          minerResponse = JSON.parse(response.body[0]);
-
-                          // TODO: Generic term that indicates the issue, and potentially indicates which dialogue to start.
-                          if ( response.body && minerResponse[0]["bp.trend"].indexOf("Raised") > -1 && minutesSinceLastAlert > config.MAX_ALERT_PERIOD) {
-
-                              request({
-                                  method: "POST",
-                                  url: config.DIALOGUE_MANAGER_URL + "/dialogue/initiate",
-                                  headers: {
-                                   "Authorization": "Basic " + new Buffer(config.USERNAME + ":" + config.PASSWORD).toString("base64")
-                                  },
-                                  json: {
-                                     // TODO: Something from the data miner response that indicates which dialogue to initiate.
-                                     "dialogueID": "2",
-                                     // TODO: Assume username on chat is the same as Patient ID in FHIR or query a service storing a mapping between the two.
-                                     "username": "user",
-                                  }
-                               },
-                               function (error, response, body) {
-
-                                   console.log("Dialogue manager: " + response.statusCode);
-
-                                   if (!error && response.statusCode == 200) {
-
-                                        console.log(response.statusCode);
-                                        lastAlert = new Date();
-                                        res.sendStatus(200);
-
-                                   } else {
-
-                                        console.log(error);
-                                        res.sendStatus(400);
-
-                                   }
-
-                               });
-
-                           } else {
-
-                               res.sendStatus(200);
-
-                           }
-
-                      } else {
-
-                          console.log(error);
-                          res.sendStatus(400);
-
-                      }
-
-                  });
-
-              });
-
-          });
+        }
 
       });
 
-   });
+    });
+
+  } else if (req.body.code.coding[0].code == config.BP_CODE) {
+
+    processObservation(req, res, function(observationHeaders, observationRow, patientHeaders, patientRow) {
+
+      request.post(config.DATA_MINER_URL + "/check/bp", {
+        json: {
+          "bp": observationHeaders.toString() + "\n" + observationRow.toString(),
+          "nn": "7",
+          "ehr": patientHeaders.toString() + "\n" + patientRow.toString()
+        },
+      },
+      function (error, response, body) {
+
+        console.log("Data miner: " + response.statusCode);
+
+        if (!error && response.statusCode == 200) {
+
+          sendAlert(response, "bp.trend", "Raised", function(status) {
+
+            res.sendStatus(status);
+
+          });
+
+        } else {
+
+          console.log(error);
+          res.sendStatus(400);
+
+        }
+
+      });
+
+    });
+
+  }
 
 });
-
-function dayOfWeekAsString(dayIndex) {
-
-  return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
-
-}
-
-function replaceAll(str, find, replace) {
-
-  return str.replace(new RegExp(find, 'g'), replace);
-
-}
-
 
 /**
  * @api {get} /:patientID/:code/:start/:end Request User information
@@ -211,48 +256,56 @@ router.get('/:patientID/:code/:start/:end', function(req, res, next) {
     header = [];
     rows = "";
 
-    JSON.parse(data).entry.forEach(function(resource) {
+    if ( data && JSON.parse(data).entry ) {
 
-      components = [];
+      JSON.parse(data).entry.forEach(function(resource) {
 
-      if ( resource.resource.component ) {
+        components = [];
 
-        components = resource.resource.component;
+        if ( resource.resource.component ) {
 
-      } else if ( resource.resource.valueQuantity ) {
+          components = resource.resource.component;
 
-        components.push(resource.resource);
+        } else if ( resource.resource.valueQuantity ) {
 
-      }
+          components.push(resource.resource);
 
-      resourceTime = new Date(resource.resource.effectiveDateTime);
+        }
 
-      row = [];
+        resourceTime = new Date(resource.resource.effectiveDateTime);
 
-      components.forEach(function(component) {
+        row = [];
 
-        var code = component.code.coding[0].code;
-        var formattedCode = "\"c" + code.replace("-", "_") + "\"";
-        var value = component.valueQuantity.value;
+        components.forEach(function(component) {
 
-        if ( !header.includes(formattedCode) ) header.push(formattedCode);
+          var code = component.code.coding[0].code;
+          var formattedCode = "\"c" + code.replace("-", "_") + "\"";
+          var value = component.valueQuantity.value;
 
-        row.push(value);
+          if ( !header.includes(formattedCode) ) header.push(formattedCode);
+
+          row.push(value);
+
+        });
+
+        row.push(resourceTime.toISOString().split('T')[0]);
+        row.push(resourceTime.toISOString().split('T')[0].substring(0, 8) + "01");
+        row.push("\"" + utils.dayOfWeekAsString(resourceTime.getDay()) + "\"");
+        rows += row + "\n";
 
       });
 
-      row.push(resourceTime.toISOString().split('T')[0]);
-      row.push(resourceTime.toISOString().split('T')[0].substring(0, 8) + "01");
-      row.push("\"" + dayOfWeekAsString(resourceTime.getDay()) + "\"");
-      rows += row + "\n";
+      header.push("\"datem\"");
+      header.push("\"date.month\"");
+      header.push("\"weekday\"\n");
 
-    });
+      res.send(utils.replaceAll(header.toString(), ",", " ") + utils.replaceAll(rows.toString(), ",", " "));
 
-    header.push("\"datem\"");
-    header.push("\"date.month\"");
-    header.push("\"weekday\"\n");
+    } else {
 
-    res.send(replaceAll(header.toString(), ",", " ") + replaceAll(rows.toString(), ",", " "));
+      res.sendStatus(404);
+
+    }
 
   });
 
