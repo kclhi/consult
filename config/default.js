e4e3d88f5b67cd91b00f663f98a9bd5831e63fc9
Martin Chapman <contact@martinchapman.co.uk>
2019-03-27 23:26:08 +0000

    Parallel dev and prod setup. LDAP containers.

--- config/default.js
+++ config/default.js
@@ -9,32 +9,22 @@ module.exports = {
 
   fhir_server: {
 
-    URL: "https://localhost/hapi/fhir",
     REST_ENDPOINT: "/"
 
   },
 
   provenance_server: {
 
-    // TODO: Add proxy entry.
-  	URL: "http://localhost:8080",
     TRACK: false
-
-  },
-
-  data_miner: {
-
-    URL: "https://localhost/mine",
 
   },
 
   dialogue_manager: {
 
-    URL: "https://localhost/dialogue",
     MAX_ALERT_PERIOD: 1
 
   },
-  
+
   terminology: {
 
     HR_CODE: "8867-4",
---
+++ config/development.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+  fhir_server: {
+
+    URL: "http://localhost:8080/hapi/fhir",
+
+  },
+
+  provenance_server: {
+
+  	URL: "http://localhost:8081",
+
+  },
+
+  data_miner: {
+
+    URL: "http://localhost:3006/mine",
+
+  },
+
+  dialogue_manager: {
+
+    URL: "http://localhost:3007/dialogue",
+
+  }
+
+};
