432da64ee23f1d1bc7867c99d0e61c97e0d0e9a0
Martin Chapman <contact@martinchapman.co.uk>
2019-11-11 16:04:37 +0000

    Explanation manager functionality outline

--- provenance_explanation_manager.py
+++ provenance_explanation_manager.py
@@ -2,3 +2,5 @@ import schemes # explanation schemes
 from explanation_manager import ExplanationManager
 
 class ProvenanceExplanationManager(ExplanationManager):
+
+# Goals: (1) Gather provenance information on recommendations given (2) Use provenance template information to provide an explanation of why these recommendations are given (XAI via provenance)
