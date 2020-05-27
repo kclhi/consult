d17e0c8d53d9093f9eb42bd9b287d563c686c06f
Martin Chapman <contact@martinchapman.co.uk>
2020-05-27 13:28:10 +0100

    Split internal and external mattermost addresses (not currently functional with certificates)

--- config/production.js
+++ config/production.js
@@ -2,8 +2,8 @@ module.exports = {
 
   mattermost: {
     // Populate with domain name information, as this is what will be received back as part of the webhook, and will need to be altered for an internal call.
-    CHAT_EXTERNAL_URL: "https://" + process.env.UI_ADDRESS + "/chat",
-    CHAT_INTERNAL_URL: "https://" + process.env.UI_ADDRESS + "/chat"
+    CHAT_INTERNAL_URL: "https://" + process.env.UI_INTERNAL_ADDRESS + "/chat",
+    CHAT_EXTERNAL_URL: "https://" + process.env.UI_ADDRESS + "/chat"
   },
 
   dialogue_manager: {
